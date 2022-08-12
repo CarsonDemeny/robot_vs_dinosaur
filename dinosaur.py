c6a9b16104e492f356dafa41fc7d315e6ba36ae1
Carson 723 <23cadememy@gmail.com>
2022-08-12 13:16:46 -0500

    Added methods and battle scene to dinosaur.py

--- dinosaur.py
+++ dinosaur.py
@@ -1,6 +1,11 @@
 class Dinosaur:
-    def __init__(self):
-        self.name = ''
-        self.health = 0
-        self.attack_power = 0
-        pass
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = attack_power
+        
+    def attack(self, robot):
+        print(f'{self.name} attacked {robot.name} and dealt {self.attack_power} damage!')
+        robot.health -= self.attack_power
+        print(f'{robot.name} has {robot.health} health remaining!')
+        

6e6dd1c29978d68c69a9280ae72b1f718d08f48d
Carson 723 <23cadememy@gmail.com>
2022-08-12 15:43:21 -0500

    Added methods add battle text to the robot.py

--- robot.py
+++ robot.py
@@ -1,6 +1,13 @@
+from weapon import Weapon
+
 class Robot:
-    def __init__(self):
-        self.name = ''
-        self.health = 0
-        self.attack_weapon = 0
-        pass
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = Weapon('Minigun', 40)
+    
+    def attack(self, dinosaur):
+        print(f'{self.name} attacked {dinosaur.name} with {self.active_weapon_name} and dealt {self.active_weapon.attack_power} damage!')
+        dinosaur.health -= self.active_weapon.attack_power
+        print(f'{dinosaur.name} has {dinosaur.health} health remaining!')
+        

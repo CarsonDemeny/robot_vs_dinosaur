c5b23fe8faa8c7905a73c32bc145bd9b50ccb52c
Carson 723 <23cadememy@gmail.com>
2022-08-15 15:53:20 -0500

    Added battle and awarded winner methods to battle.py

--- battlefield.py
+++ battlefield.py
@@ -2,11 +2,32 @@ from robot import Robot
 from dinosaur import Dinosaur
 from weapon import Weapon
 
+dinosaur = Dinosaur('Terry', 30)
+robot = Robot('Roomba')
+
 class Battlefield:
-    pass
+    def __init__(self):
+        self.run_battle()
 
+    def run_battle(self):
+        self.welcome_to_battle()
+        self.battle()
+        self.award_winner()    
 
+    def welcome_to_battle(self):
+        print('Welcome to one of the greatest battles of all time Robot Vs. Dinosaur!')
 
+    def battle(self):
+        while dinosaur.health > 0 and robot.health > 0:
+            # robot.pick_weapon()
+            robot.attack(dinosaur) 
+            dinosaur.attack(robot)
+            if robot.health == 0 or dinosaur.health == 0:
+                break
 
-# (10 points): As a developer, I want the battle to conclude once either the Robot or the Dinosaur has its health points reduced to zero.
- 
+    def award_winner(self):
+        if robot.health <= 0:
+            print(f'The dinosaurs are back once again! {dinosaur.name} Wins!')
+        elif dinosaur.health <= 0:
+            print(f'After a long hard battle robots come out on top! {robot.name} is your winner!')                
+
--- robot.py
+++ robot.py
@@ -4,10 +4,10 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon('Minigun', 40)
+        self.active_weapon = Weapon('Minigun', 20)
     
     def attack(self, dinosaur):
-        print(f'{self.name} attacked {dinosaur.name} with {self.active_weapon_name} and dealt {self.active_weapon.attack_power} damage!')
+        print(f'{self.name} attacked {dinosaur.name} with {self.active_weapon.name} and dealt {self.active_weapon.attack_power} damage!')
         dinosaur.health -= self.active_weapon.attack_power
         print(f'{dinosaur.name} has {dinosaur.health} health remaining!')
         
